b876fe5ba274c07f932bfd565d2bb6aa8d5ca410
Rohit Gupta <rohit.gupta@hs-furtwangen.de>
2014-08-21 17:01:58 +0200

    CEP Integration

--- app/src/main/java/com/de/hfu/almparser/app/ParserBroadCastReceiver.java
+++ app/src/main/java/com/de/hfu/almparser/app/ParserBroadCastReceiver.java
@@ -3,9 +3,19 @@ package com.de.hfu.almparser.app;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.StrictMode;
 import android.util.Log;
 
 import com.de.hfu.almparser.data.ALMDB;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONObject;
+
+import java.io.IOException;
 
 /**
  * Created by Rohit on 17-06-2014.
@@ -25,13 +35,50 @@ public class ParserBroadcastReceiver extends BroadcastReceiver {
             Log.d("OnReceive", "Action Received - Package Added");
             Log.d("Package name", added_package);
             saveLogEntry(action, added_package, context);
+            pushMessageToCEP(action, added_package);
 
         } else if (action.equals(Intent.ACTION_PACKAGE_REMOVED)) {
             String removed_package = intent.getData().toString();
             Log.d("OnReceive", "Action Received - Package Removed");
             Log.d("Package name", removed_package);
             saveLogEntry(action, removed_package, context);
+            pushMessageToCEP(action, removed_package);
         }
+
+    }
+
+
+    private void pushMessageToCEP(String eventType, String eventInformation) {
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+
+        StrictMode.setThreadPolicy(policy);
+
+        HttpClient httpClient = new DefaultHttpClient();
+        HttpPost post = new HttpPost("http://192.168.178.61:8080/ALMCep/webapi/myresource/");
+        post.setHeader("content-type", "application/json; charset=UTF-8");
+
+        JSONObject dato = new JSONObject();
+
+
+        try {
+            dato.put("eventType", eventType);
+            dato.put("eventInformation", eventInformation);
+
+
+            StringEntity entity = new StringEntity(dato.toString());
+            post.setEntity(entity);
+
+            HttpResponse resp = httpClient.execute(post);
+
+
+            System.out.println("OKAY!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
 
     }
 
